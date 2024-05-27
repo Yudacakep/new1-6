a8732b0559b35cc2cad86d6a6a68ee7348ef9370
Yudacakep <149886693+Yudacakep@users.noreply.github.com>
2024-05-27 20:49:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ from datetime import datetime
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
-
 from config import (
     API_HASH,
     APP_ID,
